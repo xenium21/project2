1a2f636f164bd4f5788decf382e73e82702d3d77
Noah <noahmacri1@gmail.com>
2016-05-20 22:30:45 +0800

    assert

--- project2/app.js
+++ project2/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-var asser = require('assert');
+var assert = require('assert');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
