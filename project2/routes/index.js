297ab9f8f591c40f9d51d1e51303315d11eb74c5
Noah <noahmacri1@gmail.com>
2016-05-24 18:29:54 +0800

    users

--- project2/routes/index.js
+++ project2/routes/index.js
@@ -60,8 +60,7 @@ router.get( '/hiscore', sitePages.hiscore );
 //router.get( '/elite', sitePages.elite );
 router.get( '/elite', function(req, res)
 {
-    //var played = Scores.aggregate( {$match: {username: req.user._id}}, {$group: {_id: null, count: {$sum: 1}}} );
-    var played = Scores.find({username: req.user._id}).size();
+    var played = Scores.aggregate( {$match: {username: req.user._id}}, {$group: {_id: null, count: {$sum: 1}}} );
     var hard = Scores.aggregate( {$match: {username: req.user._id, difficulty: "Hard"}}, {$group: {_id: null, count: {$sum: 1}}} );
     var medium = Scores.aggregate( {$match: {username: req.user._id, difficulty: "Medium"}}, {$group: {_id: null, count: {$sum: 1}}} );
     var easy = Scores.aggregate( {$match: {username: req.user._id, difficulty: "Easy"}}, {$group: {_id: null, count: {$sum: 1}}} );
